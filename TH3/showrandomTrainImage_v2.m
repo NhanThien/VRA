34a35bfd4f7a904c7ef23865a998372f27931952
Nhan Thien <nhan.thien@mediastep.com>
2017-10-11 20:22:44 +0700

    editNumbertest

--- TH3/showrandomTrainImage_v2.m
+++ TH3/showrandomTrainImage_v2.m
@@ -3,7 +3,7 @@ function showrandomTrainImage_v2(n)
     imgTrainAll = loadMNISTImages('train-images.idx3-ubyte');
     lblTrainAll = loadMNISTLabels('train-labels.idx1-ubyte')
 
-    nNumber = 500
+    nNumber = n;
     figure;
     img = imgTrainAll(:,nNumber);
     img2D = reshape(img,28,28);
