371b575623ef41b905ba3284ddffec3dd862f6f6
nhan <hothiennhanit@gmail.com>
2017-12-27 00:45:30 +0700

    code CNN fom kears

---
+++ Report_Final/newCNN.py
@@ -0,0 +1,122 @@
+from keras.datasets import cifar10
+from matplotlib import pyplot
+from scipy.misc import toimage
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import cv2
+import os
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+K.set_image_dim_ordering('th')
+
+# load data
+# (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# create a grid of 3x3 images
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output accuracy/loss plot")
+args = vars(ap.parse_args())
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+print("[INFO] loading images...")
+data = []
+labels = []
+imagePaths = sorted(list(paths.list_images(args["dataset"])))
+random.seed(42)
+random.shuffle(imagePaths)
+
+
+for imagePath in imagePaths:
+	# load the image, pre-process it, and store it in the data list
+	image = cv2.imread(imagePath)
+	print(imagePath)
+	image = cv2.resize(image, (32, 32))
+	image = img_to_array(image)
+	data.append(image)
+ 
+	# extract the class label from the image path and update the
+	# labels list
+	label = imagePath.split(os.path.sep)[-2]
+	label = 1 if label == "santa" else 0
+	labels.append(label)
+
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+	labels, test_size=0.25, random_state=42)
+ 
+# convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
+
+num_classes = 2
+print ("aaaaa")
+print (num_classes)
+#
+
+# model = LeNet.build(width=28, height=28, depth=3, classes=2)
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+	height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+	horizontal_flip=True, fill_mode="nearest")	
+
+# # Create the model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), padding='same', activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', kernel_constraint=maxnorm(3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+# Compile model
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+
+# sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+print(model.summary())
+#
+#print("[INFO] training network...")
+
+print("[INFO] training network...")
+# # Fit the model
+
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+	validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+	epochs=EPOCHS, verbose=1)
+ 
+# save the model to disk
+print("[INFO] serializing network...")
+model.save(args["model"])
+
+
+scores = model.evaluate(testX, testY, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
